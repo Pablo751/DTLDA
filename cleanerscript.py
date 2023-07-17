4403253d0f6ebfd49b3bd6b4d24d0a6a2cbd9da3
Pablo751 <92442092+Pablo751@users.noreply.github.com>
2023-07-17 10:19:49 +0200

    Update cleanerscript.py

--- cleanerscript.py
+++ cleanerscript.py
@@ -1,11 +1,10 @@
 import os
 import nltk
 import re
-import pickle
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
-from gensim.corpora import Dictionary, MmCorpus
-from gensim.models import LdaModel, Phrases
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel
 
 data_dir = '/Users/juanpablocasado/Downloads/20_newsgroups'  # your directory
 
@@ -19,8 +18,9 @@ for dirpath, dirnames, filenames in os.walk(data_dir):
             documents.append(text)
 
 print(f"Number of documents loaded: {len(documents)}")
-with open('number_of_documents.txt', 'w') as f:
-    f.write(f"Number of documents loaded: {len(documents)}")
+
+# Debug: Print the first document
+print(f"First document: {documents[0]}")
 
 nltk.download('stopwords')
 
@@ -36,10 +36,11 @@ def clean_document(doc):
     return words
 
 documents = [clean_document(doc) for doc in documents]
-with open('clean_documents.txt', 'w') as f:
-    for doc in documents:
-        f.write(' '.join(doc) + '\n')
 
+# Debug: Print the first cleaned document
+print(f"First cleaned document: {documents[0]}")
+
+# Download wordnet if you haven't already
 nltk.download('wordnet')
 
 stemmer = PorterStemmer()
@@ -51,30 +52,43 @@ def stem_and_lemmatize(words):
     return lemmatized
 
 documents = [stem_and_lemmatize(doc) for doc in documents]
-with open('stemmed_and_lemmatized_documents.txt', 'w') as f:
-    for doc in documents:
-        f.write(' '.join(doc) + '\n')
+
+# Debug: Print the first stemmed and lemmatized document
+print(f"First stemmed and lemmatized document: {documents[0]}")
+
+from gensim.models import Phrases
 
 bigram = Phrases(documents, min_count=5)  # at least 5 occurrences for a pair to be considered a bigram
 documents = [bigram[doc] for doc in documents]
 
+# Debug: Print the first document with bigrams
+print(f"First document with bigrams: {documents[0]}")
+
+# Create a dictionary representation of the documents
 dictionary = Dictionary(documents)
-dictionary.save('/Users/juanpablocasado/Documents/Digital text/Digital Text (f)/dictionary.pkl')
+print(f"Initial dictionary: {dictionary}")
 
+# Filter out words that occur less than 10 documents, or more than 70% of the documents
 dictionary.filter_extremes(no_below=10, no_above=0.7)
-dictionary.save('/Users/juanpablocasado/Documents/Digital text/Digital Text (f)/filtered_dictionary.pkl')
+print(f"Dictionary after filtering extremes: {dictionary}")
 
+# Bag-of-words representation of the documents
 corpus = [dictionary.doc2bow(doc) for doc in documents]
-MmCorpus.serialize('corpus.mm', corpus)
 
+# Debug: Print the first BoW representation
+print(f"First BoW representation: {corpus[0]}")
+
+# Set training parameters
 num_topics = 10  # number of topics, this can be tuned later
 chunksize = 2000
 passes = 20
 iterations = 400
 eval_every = 1
 
+# Make a index to word dictionary
 id2word = dictionary.id2token
 
+# Train the LDA model
 model = LdaModel(
     corpus=corpus,
     id2word=id2word,
@@ -86,4 +100,3 @@ model = LdaModel(
     passes=passes,
     eval_every=eval_every
 )
-model.save('/Users/juanpablocasado/Documents/Digital text/Digital Text (f)/lda_model.pkl')
